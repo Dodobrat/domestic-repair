77032b541eaac35ed22888dfe4b81a4f2da98ea5
Deyan Bozhilov <bozhilo2@uni.coventry.ac.uk>
2019-11-29 18:59:02 +0000

    added some acceptance tests

--- acceptance/techs.test.js
+++ acceptance/techs.test.js
@@ -30,20 +30,62 @@ beforeAll( async() => {
 //afterAll( () => browser.close() ) // https://github.com/GoogleChrome/puppeteer/issues/561
 
 describe('register', () => {
-	test('register user', async done => {
+	test('register technician', async done => {
 		// start generating a trace file
-		await page.tracing.start({path: 'trace/add_one_item_har.json',screenshots: true})
-		await har.start({ path: 'trace/add_one_item_trace.har' })
+		await page.tracing.start({path: 'trace/register_technician_har.json',screenshots: true})
+		await har.start({ path: 'trace/register_technician_trace.har' })
 		// ARRANGE
 		await page.goto('http://localhost:5000', { timeout: 30000, waitUntil: 'load' })
 		await page.goto('http://localhost:5000/tech/register', { timeout: 30000, waitUntil: 'load' })
 		// take a screenshot and save to the file system
-		await page.screenshot({ path: 'screenshots/empty_list.png' })
+		await page.screenshot({ path: 'screenshots/register.png' })
 
 		// ACT
 		// complete the form and click submit
 		await page.type('input[name=user]', 'dodo')
 		await page.type('input[name=email]', 'dodo@gmail.com')
+		await page.type('input[name=pass]', '123456')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('nav')
+		// await page.waitFor(1000) // sometimes you need a second delay
+
+		// ASSERT
+		const title = await page.title()
+		expect(title).toBe('Dashboard')
+
+		// extracting the text inside the first H1 element on the page
+		const heading = await page.evaluate( () => {
+			const dom = document.querySelector('nav .nav-container a span.hide-md')
+			return dom.innerText
+		})
+		expect(heading).toBe('Domestic Repair')
+
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+})
+
+
+describe('login', () => {
+	test('login technician', async done => {
+		// start generating a trace file
+		await page.tracing.start({path: 'trace/login_technician_har.json',screenshots: true})
+		await har.start({ path: 'trace/login_technician_trace.har' })
+		// ARRANGE
+		await page.goto('http://localhost:5000', { timeout: 30000, waitUntil: 'load' })
+		await page.goto('http://localhost:5000/tech/login', { timeout: 30000, waitUntil: 'load' })
+		// take a screenshot and save to the file system
+		await page.screenshot({ path: 'screenshots/login.png' })
+
+		// ACT
+		// complete the form and click submit
+		await page.type('input[name=user]', 'dodo')
 		await page.type('input[name=pass]', '123456')
 		await page.click('input[type=submit]')
 		await page.waitForSelector('nav')
